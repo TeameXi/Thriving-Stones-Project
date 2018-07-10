3daf714a7095b062893852e8f56eab0c5fe7b251
Tang Hui Xin <HuiXin.Tang@mediacorp.com.sg>
2018-07-10 09:50:21 +0800

    creation of test cases for administrative functions

--- Thriving Stones/Thriving Stones/SplashViewController.swift
+++ Thriving Stones/Thriving Stones/SplashViewController.swift
@@ -13,19 +13,23 @@ class SplashViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        // Setting background color of view
         view.backgroundColor = UIColor.init(red: 252/255, green: 252/255, blue: 244/255, alpha: 1)
+        
         splashImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Setting the animation properties of the logo
         splashImage.animationImages = [#imageLiteral(resourceName: "splash1"),#imageLiteral(resourceName: "splash2"),#imageLiteral(resourceName: "splash3"),#imageLiteral(resourceName: "splash4"),#imageLiteral(resourceName: "splash5"),#imageLiteral(resourceName: "splash6")]
         splashImage.animationDuration = 2
         splashImage.animationRepeatCount = 1
         splashImage.startAnimating()
+        
         view.addSubview(splashImage)
         
+        //Setting constraints for displayed logo
         splashImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         splashImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        splashImage.contentMode = .scaleAspectFit
     }
 
     override func didReceiveMemoryWarning() {
