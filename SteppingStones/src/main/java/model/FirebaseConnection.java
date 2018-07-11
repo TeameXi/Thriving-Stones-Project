527d54fc4710786c1b3cf870d0932ecea57878e9
Deyu <tythet.2016@smu.edu.sg>
2018-07-11 16:23:44 +0800

    Solving The Issue: Firebase App named ‘[DEFAULT]’ already exists.

--- SteppingStones/src/main/java/model/FirebaseConnection.java
+++ SteppingStones/src/main/java/model/FirebaseConnection.java
@@ -12,8 +12,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -22,12 +20,26 @@ import java.util.logging.Logger;
 public class FirebaseConnection {
     public static void initFirebase() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("src\\team-exi-thriving-stones-firebase-adminsdk-luopb-284f7a1c5a.json");
+            FileInputStream serviceAccount = new FileInputStream("C:\\Users\\DEYU\\Documents\\YR3 Term1\\IS Application Project\\Thriving-Stones-Project\\SteppingStones\\src\\team-exi-thriving-stones-firebase-adminsdk-luopb-284f7a1c5a.json");
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://team-exi-thriving-stones.firebaseio.com/")
+                    .setDatabaseUrl("https://team-exi-thriving-stones.firebaseio.com")
                     .build();
-            FirebaseApp.initializeApp(options);
+            
+            FirebaseApp firebaseApp = null;
+            List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+            if(firebaseApps!=null && !firebaseApps.isEmpty()){
+                for(FirebaseApp app : firebaseApps){
+                    if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
+                    firebaseApp = app;
+                }
+                System.out.println("Use existing firebase app");
+            }
+            else{
+                firebaseApp = FirebaseApp.initializeApp(options);
+                System.out.println("Being initizalized");
+            }
+            System.out.println(firebaseApp);
                       
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
